33ee0f7ed9b8b443badd16ccdcbac8acea79bdb0
Torjus Iveland <torjusti@gmail.com>
2018-10-31 11:32:43 +0100

    Split Pagination out into a separate component

--- client/src/components/Table/DataTable.js
+++ client/src/components/Table/DataTable.js
@@ -1,91 +1,47 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
-import { Table, Icon, Button } from 'semantic-ui-react';
-import { setPage } from '../../features/pagination/actions';
-import styles from './Table.module.css';
+import { Table } from 'semantic-ui-react';
+import Pagination from './Pagination';
+import styles from './DataTable.module.css';
 
-class DataTable extends Component {
-  handleNextPage = () => {
-    this.props.setPage(this.props.pagination.page + 1);
-  };
+const DataTable = ({ data }) => (
+  <div className={styles.container}>
+    <Pagination />
 
-  handlePrevPage = () => {
-    this.props.setPage(this.props.pagination.page - 1);
-  };
+    <div>
+      <Table>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>Artist</Table.HeaderCell>
 
-  render() {
-    return (
-      <div className={styles.container}>
-        <div className={styles.header}>
-          <Button
-            className={styles.noMargin}
-            onClick={this.handlePrevPage}
-            disabled={this.props.pagination.page === 0}
-          >
-            <Icon name="angle left" />
-            Prev
-          </Button>
-          <span className={styles.label}>
-            {`${this.props.pagination.page + 1} of ${
-              this.props.pagination.totalPages
-            }`}
-          </span>
-          <Button
-            className={styles.noMargin}
-            onClick={this.handleNextPage}
-            disabled={
-              this.props.pagination.page ===
-              this.props.pagination.totalPages - 1
-            }
-          >
-            Next
-            <Icon name="angle right" />
-          </Button>
-        </div>
-        <div>
-          <Table>
-            <Table.Header>
-              <Table.Row>
-                <Table.HeaderCell>Artist</Table.HeaderCell>
+            <Table.HeaderCell>Album</Table.HeaderCell>
 
-                <Table.HeaderCell>Album</Table.HeaderCell>
+            <Table.HeaderCell>Song</Table.HeaderCell>
 
-                <Table.HeaderCell>Song</Table.HeaderCell>
+            <Table.HeaderCell>Genre</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
 
-                <Table.HeaderCell>Genre</Table.HeaderCell>
-              </Table.Row>
-            </Table.Header>
+        <Table.Body>
+          {data.map(row => (
+            <Table.Row key={row.id}>
+              <Table.Cell>{row.artist}</Table.Cell>
 
-            <Table.Body>
-              {this.props.data.map(row => (
-                <Table.Row key={row.id}>
-                  <Table.Cell>{row.artist}</Table.Cell>
+              <Table.Cell>{row.album}</Table.Cell>
 
-                  <Table.Cell>{row.album}</Table.Cell>
+              <Table.Cell>{row.title}</Table.Cell>
 
-                  <Table.Cell>{row.title}</Table.Cell>
-
-                  <Table.Cell>{row.genre}</Table.Cell>
-                </Table.Row>
-              ))}
-            </Table.Body>
-          </Table>
-        </div>
-      </div>
-    );
-  }
-}
+              <Table.Cell>{row.genre}</Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table>
+    </div>
+  </div>
+);
 
 const mapStateToProps = state => ({
   data: state.songs,
-  pagination: state.pagination,
 });
 
-const mapDispatchToProps = {
-  setPage,
-};
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(DataTable);
+export default connect(mapStateToProps)(DataTable);
