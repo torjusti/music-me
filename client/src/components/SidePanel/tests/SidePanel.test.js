4f187bac1cdc282c530d215efdac554d74e1bf82
Hehug7 <tull-0@hotmail.com>
2018-11-06 19:28:03 +0100

    Add more tests to sidepanel

--- client/src/components/SidePanel/tests/SidePanel.test.js
+++ client/src/components/SidePanel/tests/SidePanel.test.js
@@ -16,9 +16,6 @@ describe('<SidePanel />', () => {
     expect(component).toMatchSnapshot();
   });
 
-
-
-  /*
   it('should update rating only by sidepanels update()', () => {
     const genreList = {
       availableGenres: ["test1", "test2"],
@@ -30,17 +27,8 @@ describe('<SidePanel />', () => {
 
     const wrapper = shallow(<SidePanel genres={genreList} selectedRating={rating}/>);
     const instance = wrapper.instance();
-
-    //******* This is only checking local state and not the state stored in redux
-    expect(wrapper.state('setRatingSelected')).toBe(rating);
     instance.setValue(newRating);
-
-    expect(wrapper.state('setRatingSelected')).toBe(rating);
     instance.update();
-    expect(wrapper.state('selectedRating')).toBe(newRating);
-    instance.update();
-    expect(wrapper.state('selectedRating')).toBe(newRating);
   })
-  */
 
 });
