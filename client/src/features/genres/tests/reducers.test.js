a4f3f95ebbda83cf24158d8ee76e73c5fd41a606
Hehug7 <tull-0@hotmail.com>
2018-11-08 16:22:08 +0100

    Adds redux tests for genres and pagination

---
+++ client/src/features/genres/tests/reducers.test.js
@@ -0,0 +1,44 @@
+import genres from '../reducers';
+import * as actions from '../actions';
+
+describe('genres reducer', () => {
+  it('should return an empty array initially', () => {
+    expect(genres(undefined, {})).toEqual({availableGenres: [], selectedGenres: []});
+  });
+
+  it('should set a genre', () => {
+    expect(
+      genres(
+        undefined,
+        actions.setGenreSelected(
+          ['setGenreSelected', 'genre'],
+          ['setGenreSelected'],
+        ),
+      ),
+    ).toMatchSnapshot();
+  });
+
+  it('should set no genre selected', () => {
+    expect(
+      genres(
+        undefined,
+        actions.setGenreSelected(
+          ['setGenreSelected', 'genre'],
+          [],
+        ),
+      ),
+    ).toMatchSnapshot();
+  });
+
+  it('should set all genre selected', () => {
+    expect(
+      genres(
+        undefined,
+        actions.setGenreSelected(
+          ['setGenreSelected', 'genre'],
+          ['setGenreSelected', 'genre'],
+        ),
+      ),
+    ).toMatchSnapshot();
+  });
+});
