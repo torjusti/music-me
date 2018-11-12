8f3dab419ae4fe311092ac278883754991a12c26
Hehug7 <tull-0@hotmail.com>
2018-11-12 18:41:49 +0100

    add test for sidepanel and checkboxes

--- client/src/components/SidePanel/tests/SidePanel.test.js
+++ client/src/components/SidePanel/tests/SidePanel.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount, shallow } from "enzyme";
+import { Provider } from 'react-redux';
 import SidePanel from '../SidePanel';
 import configureMockStore from 'redux-mock-store';
 
@@ -34,23 +35,32 @@ describe('SidePanel', () => {
     };
 
     store = mockStore(initialState);
-
-    // Shallow render the container passing in the mock store
-    wrapper = shallow(<SidePanel store={store} />);
+    wrapper = mount(
+      <Provider store={store}>
+        <SidePanel />
+      </Provider>,
+    );
   });
 
   it('should render correctly', () => {
+    wrapper = shallow(<SidePanel store={store} />);
     expect(wrapper.dive()).toMatchSnapshot();
     expect(wrapper.dive().length).toEqual(1);
   });
 
   it('should have 1 as selectedRating', () => {
+    wrapper = shallow(<SidePanel store={store} />);
     // test that the state values were correctly passed as props
     expect(wrapper.props().rating.selectedRating).toBe(1);
   });
 
   it('should have ratingEnabled equal to false', () => {
+    wrapper = shallow(<SidePanel store={store} />);
     // test that the state values were correctly passed as props
     expect(wrapper.props().rating.ratingEnabled).toBe(false);
+  });
+
+  it('should have two checkboxes', () => {
+    expect(wrapper.find('ul').children().length).toBe(2);
   });
 });
