a52e99bec6070c52527ee79ae2d8d90ce77d5ce1
Hehug7 <tull-0@hotmail.com>
2018-11-12 19:19:20 +0100

    Refactor pagination test

--- client/src/components/Table/tests/Pagination.test.js
+++ client/src/components/Table/tests/Pagination.test.js
@@ -1,20 +1,44 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount, shallow } from "enzyme";
+import { Provider } from 'react-redux';
+import Pagination from '../Pagination';
+import configureMockStore from 'redux-mock-store';
 
-import { Pagination } from '../Pagination';
+describe('Pagination', () => {
 
-describe('<Pagination />', () => {
-  it('renders pagination', () => {
-    const wrapper = shallow(
-      <Pagination pagination={{ page: 1, totalPages: 5 }} />,
+  // Create the mock store
+  const mockStore = configureMockStore();
+
+  let wrapper, store;
+
+  beforeEach(() => {
+
+    const initialState = {
+      pagination: {
+        page: 1,
+        totalPages: 5
+      }
+    };
+
+    store = mockStore(initialState);
+    wrapper = mount(
+      <Provider store={store}>
+        <Pagination />
+      </Provider>,
     );
-    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('renders pagination', () => {
+    wrapper = shallow(
+      <Pagination store={store}/>,
+    );
+    expect(wrapper.dive()).toMatchSnapshot();
   });
 
   it('renders two buttons', () => {
-    const wrapper = shallow(
-      <Pagination pagination={{ page: 1, totalPages: 5 }} />,
+    wrapper = shallow(
+      <Pagination store={store} />,
     );
-    expect(wrapper.find('Button').length).toEqual(2);
+    expect(wrapper.dive().find('Button').length).toEqual(2);
   });
 });
